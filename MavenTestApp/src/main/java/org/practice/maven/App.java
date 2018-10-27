111c009f4e0ee3d816ce6f47302034c38f7890d5
Balu-Hadoop <vbks87@gmail.com>
2018-10-27 20:36:45 +0530

    removed  commenting

--- MavenTestApp/src/main/java/org/practice/maven/App.java
+++ MavenTestApp/src/main/java/org/practice/maven/App.java
@@ -9,8 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-		/* for(int i=1; i<=10; i++){
+		for(int i=1; i<=10; i++){
 			System.out.println(i);
-		}*/
+		}
     }
 }
