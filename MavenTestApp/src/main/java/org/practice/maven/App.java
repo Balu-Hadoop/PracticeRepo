412b92f38dbd98fbad5876d71732e814369c84e3
Balu-Hadoop <vbks87@gmail.com>
2018-10-27 19:20:20 +0530

    Added Maven Sample Project

---
+++ MavenTestApp/src/main/java/org/practice/maven/App.java
@@ -0,0 +1,16 @@
+package org.practice.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		for(int i=1; i<=10; i++){
+			System.out.println(i);
+		}
+    }
+}
